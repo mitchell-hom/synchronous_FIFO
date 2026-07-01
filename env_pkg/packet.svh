c6156104f777e5ab726a8a783165251858776bc1
mitchell-hom <131689330+mitchell-hom@users.noreply.github.com>
2026-06-30 21:21:58 -0400

    added constraints

--- env_pkg/packet.svh
+++ env_pkg/packet.svh
@@ -12,6 +12,9 @@ class packet extends uvm_sequence_item;
         bit EMPTY;
         bit RD_ACK;
         bit RD_ERR;
+  
+  		// testbench-related
+  		rand bit err_injection; 
 
 	// get all the bells n whistles (methods) for these guys
 	`uvm_object_utils_begin(packet)
@@ -34,6 +37,27 @@ class packet extends uvm_sequence_item;
 		super.new(name);
 	endfunction
   
+  	constraint c_error_rate {
+      err_injection dist {1'b0 := 80, 1'b1 := 20};
+  	}
+  
+  	constraint c_legal_cmd {
+      err_injection == 0 -> {
+        if (SINIT == 1) {
+              WR_EN == 0;
+              RD_EN == 0;
+        }
+
+        if (WR_EN == 1) {
+              RD_EN == 0;
+        }
+
+        if (RD_EN == 1) {
+              WR_EN == 0;
+        }
+      }
+  	}
+  
   	// supporting functions
   	function bit is_write();
       	if (!SINIT && WR_EN && !RD_EN) begin
@@ -50,4 +74,32 @@ class packet extends uvm_sequence_item;
           	return 0;
         end
     endfunction : is_read
+  
+  	function bit is_wr_error();
+      	if (WR_EN) begin
+          	if (DATA_COUNT == global_constants::DEPTH) begin
+            	return 1;
+            end else if (SINIT == 1) begin
+              	return 1;
+            end else if (RD_EN == 1) begin
+              	return 1;
+            end
+      	end else begin
+          	return 0;
+        end
+  	endfunction : is_wr_error
+  
+    function bit is_rd_error();
+      	if (RD_EN) begin
+          	if (DATA_COUNT == 0) begin
+            	return 1;
+            end else if (SINIT == 1) begin
+              	return 1;
+            end else if (WR_EN == 1) begin
+              	return 1;
+            end
+      	end else begin
+          	return 0;
+        end
+  	endfunction : is_rd_error
 endclass : packet
