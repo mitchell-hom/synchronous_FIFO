cc99f0492114d2b8b86fb159c368a21bbefd9d49
mitchell-hom <131689330+mitchell-hom@users.noreply.github.com>
2026-06-30 20:25:13 -0400

    added more coverage

--- env_pkg/sequences/seq_write.svh
+++ env_pkg/sequences/seq_write.svh
@@ -1,63 +1,82 @@
 class seq_write extends uvm_sequence #(packet);
-  `uvm_object_utils (seq_write)
+  	`uvm_object_utils (seq_write)
 
 	packet pkt;
 
-  function new (string name="seq_write");
+  	function new (string name="seq_write");
 		super.new(name);
 	endfunction
 
 	virtual task body();
-      `uvm_info("seq_write", "Starting body of write sequence", UVM_LOW)
-		
-		// create stimulus
-		pkt = packet::type_id::create("pkt");
-		
-		// drive for the prescribed number of clock cycles
-		start_item(pkt);
+    	`uvm_info("seq_write", "Starting body of write sequence", UVM_LOW)
       
-      	// turn off randomization
-      	pkt.rand_mode(0);
+     	multi_wr();
+      	multi_rd();
+      	device_rst();
+      	intlv();
+	endtask : body
+  
+  	// multiple writes in a row
+  	// also tests overflow
+  	virtual task multi_wr();
+      `uvm_info("SEQ_WRITE", "Starting multi write", UVM_LOW)
 
-      	// set contents of packet
-		pkt.SINIT = 0;
-		pkt.DIN = '0;
-		pkt.WR_EN = '0;
-		pkt.RD_EN = '0;
-
-		finish_item(pkt);
-
-      	// WRITE operation
-      	for (int i = 0; i < global_constants::BUS_WIDTH; i++) begin
-        	// need to instantiate every time
-        	pkt = packet::type_id::create("pkt");
-        	start_item(pkt);
-          	pkt.rand_mode(0);
-        	pkt.WR_EN = 1;
-        	pkt.DIN = i;
-        	finish_item(pkt);
-      	end
-      
-      	// READ operation
-      	for (int i = 0; i < global_constants::BUS_WIDTH; i++) begin
-        	pkt = packet::type_id::create("pkt");
-        	start_item(pkt);
-          	pkt.rand_mode(0);
-        	pkt.WR_EN = 0;
-        	pkt.RD_EN = 1;
-        	pkt.DIN = '0;
-        	finish_item(pkt);
-      	end
-      
-      	// constrained random
-      	// TODO: constraints
-      	repeat(10) begin
-        	pkt = packet::type_id::create("pkt");
+      	for (int i = 0; i < global_constants::DEPTH; i++) begin
+          	pkt = packet::type_id::create("pkt");
           	start_item(pkt);
-          	assert(pkt.randomize());
+          	assert(pkt.randomize() with {SINIT == 0; WR_EN == 1; RD_EN == 0;});
           	finish_item(pkt);
       	end
-		
-      `uvm_info("seq_write", "Write sequence complete", UVM_LOW);
-	endtask : body
+    endtask : multi_wr
+  
+  	// multiple reads in a row
+  	virtual task multi_rd();
+      	`uvm_info("SEQ_READ", "Starting multi read", UVM_LOW)
+
+      	for (int i = 0; i < global_constants::DEPTH; i++) begin
+          	pkt = packet::type_id::create("pkt");
+          	start_item(pkt);
+          	assert(pkt.randomize() with {SINIT == 0; RD_EN == 1; WR_EN == 0;});
+          	finish_item(pkt);
+      	end
+    endtask : multi_rd
+  
+  	// basic reset
+  	virtual task device_rst();
+    	`uvm_info("SEQ_RST", "Starting device reset", UVM_LOW)
+
+		pkt = packet::type_id::create("pkt");
+    	start_item(pkt);
+    
+    	pkt.rand_mode(0);
+    	pkt.WR_EN = 0;
+    	pkt.RD_EN = 0;
+    	pkt.SINIT = 1;
+    	
+    	finish_item(pkt);
+    
+    	pkt = packet::type_id::create("pkt");
+    	start_item(pkt);
+    
+    	pkt.rand_mode(0);
+    	pkt.WR_EN = 0;
+    	pkt.RD_EN = 0;
+    	pkt.SINIT = 0;
+    	
+    	finish_item(pkt);
+  	endtask : device_rst
+  
+  	// interleaved
+  	virtual task intlv();
+      	`uvm_info("SEQ_INTLV", "Starting interleave", UVM_LOW)
+
+      	repeat(10) begin
+       		assert(pkt.randomize() with {SINIT == 0; WR_EN == 1; RD_EN == 0;});
+          	assert(pkt.randomize() with {SINIT == 0; RD_EN == 1; WR_EN == 0;});
+      	end
+    endtask : intlv
+  
+  	// TODO: acknowledges not always compliments
+  	// TODO: random
+  	// TODO: fig. 2?
 endclass : seq_write
