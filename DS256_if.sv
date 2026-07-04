37a86e6053611de7200dbd4997f3c389bc63e5f2
mitchell-hom <131689330+mitchell-hom@users.noreply.github.com>
2026-07-03 23:13:57 -0400

    added clocking block
    needed to add this to specify when to sample outputs of TB

--- DS256_if.sv
+++ DS256_if.sv
@@ -10,4 +10,24 @@ interface DS256_if(input bit CLK, input bit SINIT);
 	logic EMPTY;
 	logic RD_ACK;
 	logic RD_ERR;
+  
+  	// helps to sample at the correct part of timestep
+  clocking cb @(posedge CLK);
+    	// define when things get sampled
+    	default input #1step output #0;
+    
+    	// testbench outputs
+    	output WR_EN;
+    	output RD_EN;
+    
+    	// testbench inputs
+      	input FULL; 
+      	input DATA_COUNT;
+      	input WR_ACK;
+      	input WR_ERR;
+      	input DOUT;
+      	input EMPTY;
+      	input RD_ACK;
+      	input RD_ERR;
+    endclocking : cb
 endinterface : DS256_if
