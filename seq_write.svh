c809a8d16cc6ce950c8f51c3e9cfe072e89cc4da
mitchell-hom <131689330+mitchell-hom@users.noreply.github.com>
2026-07-04 22:45:35 -0400

    added basic operations

--- seq_write.svh
+++ seq_write.svh
@@ -12,39 +12,17 @@ class seq_write extends uvm_sequence #(packet);
       
      	multi_wr();
       	multi_rd();
-      	device_rst();
-      	intlv();
-      	il_rq();
-     	rand_cmds();
+      	// TODO: uncomment
+      	//device_rst();
+      	//intlv();
+      	//il_rq();
+     	//rand_cmds();
 	endtask : body
+ 	
   
-  	// multiple writes in a row
-  	// also tests overflow
-  	virtual task multi_wr();
-      `uvm_info("SEQ_WRITE", "Starting multi write", UVM_LOW)
-
-      	for (int i = 0; i < global_constants::DEPTH; i++) begin
-          	pkt = packet::type_id::create("pkt");
-          	start_item(pkt);
-          	assert(pkt.randomize() with {SINIT == 0; WR_EN == 1; RD_EN == 0;});
-          	finish_item(pkt);
-      	end
-    endtask : multi_wr
-  
-  	// multiple reads in a row
-  	virtual task multi_rd();
-      	`uvm_info("SEQ_READ", "Starting multi read", UVM_LOW)
-
-      	for (int i = 0; i < global_constants::DEPTH; i++) begin
-          	pkt = packet::type_id::create("pkt");
-          	start_item(pkt);
-          	assert(pkt.randomize() with {SINIT == 0; RD_EN == 1; WR_EN == 0;});
-          	finish_item(pkt);
-      	end
-    endtask : multi_rd
-  
-  	// basic reset
-  	virtual task device_rst();
+  	// BASIC OPERATION
+    // basic reset
+  	virtual task RESET();
     	`uvm_info("SEQ_RST", "Starting device reset", UVM_LOW)
 
 		pkt = packet::type_id::create("pkt");
@@ -66,22 +44,72 @@ class seq_write extends uvm_sequence #(packet);
     	pkt.SINIT = 0;
     	
     	finish_item(pkt);
-  	endtask : device_rst
+  	endtask : RESET
   
+  	// basic write operation
+  	task WRITE(input din=0, input is_valid=0);
+    	pkt = packet::type_id::create("pkt");
+      	start_item(pkt);
+      	
+    	// default: randomize data
+      	if (is_valid == 0) begin
+    		if (!pkt.randomize() with {SINIT == 0; WR_EN == 1; RD_EN == 0;}) begin
+          		`uvm_fatal("SEQ_RAND", "Could not randomize packet in WRITE")
+			end
+        // don't randomize, set as specified value
+        end else begin
+        	if (!pkt.randomize() with {SINIT == 0; WR_EN == 1; RD_EN == 0; DIN == din;}) begin
+          		`uvm_fatal("SEQ_RAND", "Could not randomize packet in WRITE")
+			end
+        end
+    
+      	finish_item(pkt);
+    endtask : WRITE
+      
+  	// basic read operation
+  	task READ();
+    	pkt = packet::type_id::create("pkt");
+      	start_item(pkt);
+      	
+      	pkt.SINIT = 0;
+      	pkt.WR_EN = 0;
+      	pkt.RD_EN = 1;
+      	pkt.DIN = 0;
+
+      	finish_item(pkt);
+    endtask : READ
+  
+  
+  
+  	// OTHER SEQUENCES ////
+  	// multiple writes in a row
+  	// also tests overflow
+  	virtual task multi_wr();
+      	`uvm_info("SEQ_WRITE", "Starting multi write", UVM_LOW)
+
+      	// do writes until full, then overflow it 
+      	repeat(global_constants::DEPTH + 1) begin
+        	WRITE();
+      	end
+    endtask : multi_wr
+  
+  	// multiple reads in a row
+  	virtual task multi_rd();
+      	`uvm_info("SEQ_READ", "Starting multi read", UVM_LOW)
+
+      	// do reads until empty, then continue
+        repeat(global_constants::DEPTH + 1) begin
+          	READ();
+      	end
+    endtask : multi_rd
+
   	// interleaved
   	virtual task intlv();
       	`uvm_info("SEQ_INTLV", "Starting interleave", UVM_LOW)
 
       	repeat(10) begin
-          	pkt = packet::type_id::create("pkt");
-          	start_item(pkt);
-       		assert(pkt.randomize() with {SINIT == 0; WR_EN == 1; RD_EN == 0;});
-          	finish_item(pkt);
-          
-          	pkt = packet::type_id::create("pkt");
-          	start_item(pkt);
-          	assert(pkt.randomize() with {SINIT == 0; RD_EN == 1; WR_EN == 0;});
-          	finish_item(pkt);
+          	WRITE();
+          	READ();
       	end
     endtask : intlv
   
@@ -94,10 +122,7 @@ class seq_write extends uvm_sequence #(packet);
       
       	// background writes
       	repeat(global_constants::DEPTH - 1) begin
-      		pkt = packet::type_id::create("pkt");
-          	start_item(pkt);
-      		assert(pkt.randomize() with {SINIT == 0; WR_EN == 1; RD_EN == 0;});
-          	finish_item(pkt);
+          	WRITE();
         end
 
       	// start actual illegal commands
@@ -105,21 +130,23 @@ class seq_write extends uvm_sequence #(packet);
         pkt = packet::type_id::create("pkt");
       	start_item(pkt);
       	pkt.c_legal_cmd.constraint_mode(0);
-		assert(pkt.randomize() with {SINIT == 0; WR_EN == 1; RD_EN == 1;});
+      	if (!pkt.randomize() with {SINIT == 0; WR_EN == 1; RD_EN == 1;}) begin
+        	`uvm_fatal("SEQ_RAND", "Could not randomize packet in sequence il_rq");
+      	end
       	finish_item(pkt);
       
       	// SINIT high while read is enabled
         pkt = packet::type_id::create("pkt");
         start_item(pkt);
         pkt.c_legal_cmd.constraint_mode(0);
-      	assert(pkt.randomize() with {SINIT == 1; RD_EN == 1; WR_EN == 0;});            	finish_item(pkt);
+      	if (!pkt.randomize() with {SINIT == 1; RD_EN == 1; WR_EN == 0;}) begin
+          `uvm_fatal("SEQ_RAND", "Could not randomize packet in sequence il_rq")
+      	end
+        finish_item(pkt);
 
       	
 		// background read
-      	pkt = packet::type_id::create("pkt");
-      	start_item(pkt);
-      	assert(pkt.randomize() with {SINIT == 0; WR_EN == 0; RD_EN == 1;});
-      	finish_item(pkt);
+      	READ();
     endtask : il_rq
   
   	// TODO: random
@@ -129,7 +156,9 @@ class seq_write extends uvm_sequence #(packet);
       repeat(100) begin
           	pkt = packet::type_id::create("pkt");
         	start_item(pkt);
-          	assert(pkt.randomize());
+        	if (!pkt.randomize()) begin
+              `uvm_fatal("SEQ_RAND", "Could not randomize packet in sequence rand_cmds")
+        	end
         	finish_item(pkt);
       	end
     endtask : rand_cmds
