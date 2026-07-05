972593118d1196758059618bb8ba41753cce9a9b
mitchell-hom <131689330+mitchell-hom@users.noreply.github.com>
2026-07-04 22:44:52 -0400

    Update scoreboard.svh

--- scoreboard.svh
+++ scoreboard.svh
@@ -15,47 +15,32 @@ class scoreboard extends uvm_scoreboard;
 	// phases
 	virtual function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
-		
-      	// instantiate analysis implementation
-      	//mAI = new("mAI", this);
 	endfunction : build_phase
-  
-	// TODO: checking for handshaking signals
-  	virtual function void check_phase(uvm_phase phase);
-    	super.check_phase(phase);
-   
-      	`uvm_info("SB_CHECK", "starting check phase", UVM_LOW)
-      
-      	// iterate through captured packets
-      	foreach (pkts[i]) begin
-          	// track WRITE operations
-			// sync to clock edge done in monitori
-          	if (pkts[i].is_write()) begin
-              data.push_back(pkts[i].DIN);
-                               
-        	// track READ operations
-            end else if (pkts[i].is_read()) begin
+       	
+	// checking
+  	virtual function void write(packet pkt);
+      	// RESET
+      	if (pkt.SINIT) begin
+          	// clear queue
+        	data.delete();
+          
+  		// TODO: handshaking?
+        // TODO: data offset by one clock edge
+        // everything else
+      	end else begin
+        	// write
+          	if (pkt.WR_EN) begin
+              	data.push_back(pkt.DIN);
+            end else if (pkt.RD_EN) begin
               	compare = data.pop_front();
               
-              	// do comparison
-              	if (pkts[i].DOUT != compare) begin
-            		`uvm_error("SB_READ", $sformatf("Incorrect data received. Expected: %b Actual: %b", compare, pkts[i].DOUT))
+                // do comparison
+              	if (pkt.DOUT != compare) begin
+            		`uvm_error("SB_READ", $sformatf("Incorrect data received. Expected: %b Actual: %b", compare, pkt.DOUT))
 				end else begin
-                  `uvm_info("SB_READ", $sformatf("Data received. Expected: %04b Actual: %b", 4'(compare), pkts[i].DOUT), UVM_LOW)
+                  `uvm_info("SB_READ", $sformatf("Data received. Expected: %04b Actual: %b", 4'(compare), pkt.DOUT), UVM_LOW)
                 end // else
-            end // else if
-          
-          	// TODO handshaking
-    	end // foreach
-	endfunction : check_phase
-    
-  	
-	// other functions
-  	// capture data for later comparison
-  	virtual function void write(packet pkt);
-      	packet pkt_cpy;
-      	$cast(pkt_cpy, pkt.clone());
-      
-      	pkts.push_back(pkt_cpy);
+            end
+        end
     endfunction : write
 endclass : scoreboard
