137e7edc278e8ef55485080759f44528bb98e359
mitchell-hom <131689330+mitchell-hom@users.noreply.github.com>
2026-07-15 18:23:35 -0400

    Update DS256.sv

--- DS256.sv
+++ DS256.sv
@@ -1,54 +1,107 @@
-`timescale 1ns/1ps
+// synchronous FIFO
+// depth: 4
+// bus width: 4
 
-import uvm_pkg::*;
-`include "uvm_macros.svh"
-import DS256_env_pkg::*;
+/* NOTE: this design assumes that 1 timestep is significantly less than the period of the clock
+this is because of the flagging for the FIFO being full, which updates the timestep AFTER
+the data is latched */
 
-module tb_top;
-	logic clk, sinit;
-  
-  	// clock generation at specified frequency
-  always #(global_constants::PERIOD / 2) clk =~ clk;
+module DS256 (	input [global_constants::BUS_WIDTH-1:0] DIN,
+		input WR_EN,
+		input RD_EN,
+		input CLK,
+		input SINIT,
+		output reg FULL,
+		output reg [global_constants::COUNT_BITS-1:0] DATA_COUNT, 
+		output reg WR_ACK,
+		output reg WR_ERR,
+		output reg [global_constants::BUS_WIDTH-1:0] DOUT,
+		output reg EMPTY,
+		output reg RD_ACK,
+		output reg RD_ERR);
 
-	DS256_if Iif(clk, sinit);
-	DS256 Idut(	.CLK(Iif.CLK), 
-			.DIN(Iif.DIN),
-			.WR_EN(Iif.WR_EN),
-			.RD_EN(Iif.RD_EN),
-			.SINIT(Iif.SINIT),
-			.FULL(Iif.FULL),
-			.DATA_COUNT(Iif.DATA_COUNT),
-			.WR_ACK(Iif.WR_ACK),
-			.WR_ERR(Iif.WR_ERR),
-			.DOUT(Iif.DOUT),
-			.EMPTY(Iif.EMPTY),
-			.RD_ACK(Iif.RD_ACK),
-			.RD_ERR(Iif.RD_ERR));
-	base_test Itest;
-  
-  	// run verification
-  	initial begin
-      uvm_config_db#(virtual DS256_if)::set(null, "*", "DS256_if", Iif);
-      run_test("base_test");
-    end
+	bit [global_constants::COUNT_BITS-1:0] count;
+	bit [global_constants::DEPTH-1:0] rPtr, wPtr;
+	reg [global_constants::DEPTH-1:0] array [global_constants::BUS_WIDTH-1:0];
+	reg fullFlag, emptyFlag;
 
-  	// initialize values
-  	initial begin
-      	clk = 0;
-      	sinit = 1;
-      
-      	// testing this
-      	Iif.DIN = '0;
-      	Iif.WR_EN = 0;
-      	Iif.RD_EN = 0;
-      
-      	#(global_constants::PERIOD);
-      	sinit = 0;
-    end      	
+	always_ff @(posedge CLK) begin
+		// SINIT; synchronous reset
+		if (SINIT) begin
+			array <= '{global_constants::DEPTH{'0}}; // clear memory
+			// reset pointers
+			wPtr <= 0; 
+			rPtr <= 0;
+			count <= 0;	
 
-	// write wave data to output file
-	initial begin
-      	$dumpfile("dump.vcd");
-		$dumpvars(); 
-	end 
-endmodule : tb_top
+			// reset output pins
+			DATA_COUNT <= 0;		
+			EMPTY <= 1;
+			FULL <= 0;
+
+		// no SINIT; normal operation
+		end else begin	
+          	if (count == '0) begin
+            	FULL <= 0;
+              	EMPTY <= 1;
+            end else if (count == global_constants::DEPTH) begin
+              	FULL <= 1;
+              	EMPTY <= 0;
+            end
+          
+			// WRITE operation
+			if (WR_EN) begin
+				// ERRORS
+				if (fullFlag | RD_EN) begin
+					WR_ERR <= 1;
+					WR_ACK <= 0;
+				// no error; do a write
+				end else begin
+					// write
+					array[wPtr] <= DIN;
+
+					// update pointer
+					wPtr <= wPtr + 1;
+					count <= count + 1;
+
+					// set output pins
+					WR_ACK <= 1;
+					WR_ERR <= 0;		
+				end
+			end // WRITE operation
+
+			// READ operation
+			if (RD_EN) begin
+				// ERRORS
+				if (emptyFlag | WR_EN) begin
+					RD_ERR <= 1;
+					RD_ACK <= 0;
+				// no error; do a read
+				end else begin
+					// read
+					DOUT <= array[rPtr];
+
+					// update pointer
+					rPtr <= rPtr + 1;
+					count <= count - 1;
+
+					// set output pins
+					RD_ACK <= 1;
+					RD_ERR <= 0;
+				end 
+			end // WRITE operation
+		end // normal operation (non-reset)	
+	end // always_ff...
+
+	// TODO can't write to same variable in combinational block and sequential
+	// block, so need to implement this elsewhere
+	/*
+	always_comb begin
+		fullFlag = count == global_constants::DEPTH - 1;
+		FULL = fullFlag;
+
+		emptyFlag = count == 0;
+		EMPTY = emptyFlag;
+	end // always_comb
+	*/
+endmodule : DS256;
