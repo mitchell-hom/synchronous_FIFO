8748753e599a7e692493112a4a055ab20533486c
mitchell-hom <131689330+mitchell-hom@users.noreply.github.com>
2026-07-16 19:07:03 -0400

    update count bits

--- global_constants.svh
+++ global_constants.svh
@@ -1,7 +1,7 @@
 package global_constants;
 	// parameters for this specific implementation
 	parameter int BUS_WIDTH = 4;
-	parameter int COUNT_BITS = 2; // amount of bits required to represent amount of data in FIFO
+	parameter int COUNT_BITS = 3; // amount of bits required to represent amount of data in FIFO
 	parameter int DEPTH = 4;
 	parameter int PERIOD = 6; // table 6: 6.3 ns period chosen for data width=8 (closest)
 
